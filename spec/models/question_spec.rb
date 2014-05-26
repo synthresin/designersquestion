cddf73f62a8435ab067ad0aa5765edf14d69352b
synthresin <synthresin@bagseong-wan-ui-MacBook-Pro.local>
2014-05-26 23:03:38 +0900

    첫번째 테스트 작성

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_helper'
 
 describe Question do
-  it '제목, 내용, user_id가 있으면 유효하다.'
+  it '제목, 내용, user_id가 있으면 유효하다.' do
+  	question = Question.new(
+  		title:'제목',
+  		lastname:'내용',
+  		user_id: 1)
+  	expect(question).to be_valid
+  end
   it '제목이 없으면 유효하지 않다.'
   it '내용이 없으면 유효하지 않다.'
   it 'user_id가 없으면 유효하지 않다.'
