d723cb98ca0a1b10ae469a6543ea8141d67bfb4e
synthresin <synthresin@bagseong-wan-ui-MacBook-Pro.local>
2014-05-26 23:11:42 +0900

    Question spec 추가 작성

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -8,7 +8,13 @@ describe Question do
   		user_id: 1)
   	expect(question).to be_valid
   end
-  it '제목이 없으면 유효하지 않다.'
-  it '내용이 없으면 유효하지 않다.'
-  it 'user_id가 없으면 유효하지 않다.'
+  it '제목이 없으면 유효하지 않다.' do
+  	expect(Question.new(title: nil)).to have(1).errors_on(:title)
+ 	end
+  it '내용이 없으면 유효하지 않다.'  do
+  	expect(Question.new(body: nil)).to have(1).errors_on(:title)
+ 	end
+  it 'user_id가 없으면 유효하지 않다.' do
+  	expect(Question.new(user_id: nil)).to have(1).errors_on(:title)
+  end
 end
