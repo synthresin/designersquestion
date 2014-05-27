bc2d6748f095f3492fc6fa1df3b2f150d69852fc
synthresin <synthresin@bagseong-wan-ui-MacBook-Pro.local>
2014-05-27 14:08:52 +0900

    question spec 을 factory 사용하도록 변경

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -4,5 +4,6 @@ FactoryGirl.define do
   factory :question do
     title "MyString"
     body "MyText"
+    user_id 1
   end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,20 +1,20 @@
 require 'spec_helper'
 
 describe Question do
-  it '제목, 내용, user_id가 있으면 유효하다.' do
-  	question = Question.new(
-  		title:'제목',
-  		body:'내용',
-  		user_id: 1)
+  it '유효한 팩토리를 가지고 있다.' do
+  	question = FactoryGirl.build(:question)
   	expect(question).to be_valid
   end
   it '제목이 없으면 유효하지 않다.' do
-  	expect(Question.new(title: nil)).to have(1).errors_on(:title)
+    question = FactoryGirl.build(:question, title: nil)
+  	expect(question).to have(1).errors_on(:title)
  	end
-  it '내용이 없으면 유효하지 않다.'  do
-  	expect(Question.new(body: nil)).to have(1).errors_on(:title)
+  it '내용이 없으면 유효하지 않다.' do
+    question = FactoryGirl.build(:question, body: nil)
+  	expect(question).to have(1).errors_on(:body)
  	end
   it 'user_id가 없으면 유효하지 않다.' do
-  	expect(Question.new(user_id: nil)).to have(1).errors_on(:title)
+    question = FactoryGirl.build(:question, user_id: nil)
+  	expect(question).to have(1).errors_on(:user_id)
   end
 end
