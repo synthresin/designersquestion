609f9168a8fb4dfdb31636154761c90ee2bd0039
synthresin <synthresin@bagseong-wan-ui-MacBook-Pro.local>
2014-05-27 16:27:11 +0900

    question factory 에 faker 적용

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,9 +1,10 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
+require 'faker'
 
 FactoryGirl.define do
   factory :question do
-    title "MyString"
-    body "MyText"
+    title { Faker::Lorem.sentence }
+    body { Faker::Lorem.paragraph }
 
     association :user
   end
