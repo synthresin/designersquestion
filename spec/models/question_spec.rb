5a06ebdda1e0fde2fe1044699f910a623e8c1075
synthresin <synthresin@bagseong-wan-ui-MacBook-Pro.local>
2014-05-27 14:14:16 +0900

    spec 단순화

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,19 +2,19 @@ require 'spec_helper'
 
 describe Question do
   it '유효한 팩토리를 가지고 있다.' do
-  	question = FactoryGirl.build(:question)
+  	question = build(:question)
   	expect(question).to be_valid
   end
   it '제목이 없으면 유효하지 않다.' do
-    question = FactoryGirl.build(:question, title: nil)
+    question = build(:question, title: nil)
   	expect(question).to have(1).errors_on(:title)
  	end
   it '내용이 없으면 유효하지 않다.' do
-    question = FactoryGirl.build(:question, body: nil)
+    question = build(:question, body: nil)
   	expect(question).to have(1).errors_on(:body)
  	end
   it 'user_id가 없으면 유효하지 않다.' do
-    question = FactoryGirl.build(:question, user_id: nil)
+    question = build(:question, user_id: nil)
   	expect(question).to have(1).errors_on(:user_id)
   end
 end
