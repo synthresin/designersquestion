4e1ab2c834a9ce8ac483178159d165491ee7b1af
synthresin <synthresin@bagseong-wan-ui-MacBook-Pro.local>
2014-05-28 11:10:29 +0900

    공백 제거

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,7 +6,7 @@ FactoryGirl.define do
   	password_confirmation "3wzzzgax"
   	#sequence(:email) { |n| "synthresin#{n}@me.com"} 
   	email { Faker::Internet.email }
-  	
+
   	association :profile
   end
 end
