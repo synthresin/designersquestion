4a892dbe9e8ab86b2247547a398bf663025c76a9
synthresin <synthresin@bagseong-wan-ui-MacBook-Pro.local>
2014-06-01 00:46:04 +0900

    page controller 생성 및 스펙 작성 시작

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140526144443) do
+ActiveRecord::Schema.define(version: 20140530132855) do
 
   create_table "answers", force: true do |t|
     t.text     "body"
@@ -41,6 +41,12 @@ ActiveRecord::Schema.define(version: 20140526144443) do
   end
 
   add_index "questions", ["user_id"], name: "index_questions_on_user_id", using: :btree
+
+  create_table "tags", force: true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
---
+++ spec/controllers/page_controller_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe PageController do
+	describe 'GET #index' do
+		it "question 의 배열을 populate 한다."
+		it ":index 템플릿을 렌더 한다."
+	end
+end
